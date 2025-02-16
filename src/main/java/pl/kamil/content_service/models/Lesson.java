235a7fefda5db628752e548487c6c5d9d14b997c
KamilMad <kamilul085@gmail.com>
2025-02-16 23:18:40 +0100

    Add: Lesson entity

---
+++ src/main/java/pl/kamil/content_service/models/Lesson.java
@@ -0,0 +1,23 @@
+package pl.kamil.content_service.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "lessons")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+}
