d64d6825fd67bc75823a35a224621b8bc1869e15
KamilMad <kamilul085@gmail.com>
2025-02-17 14:31:10 +0100

    Add: uploadFile endpoint

--- src/main/java/pl/kamil/content_service/controllers/LessonController.java
+++ src/main/java/pl/kamil/content_service/controllers/LessonController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import pl.kamil.content_service.dtos.LessonRequest;
 import pl.kamil.content_service.models.Lesson;
 import pl.kamil.content_service.services.LessonService;
 
