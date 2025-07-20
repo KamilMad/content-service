0cc639de6fba93fa08f457a51243113c60ba3512
KamilMad <kamilul085@gmail.com>
2025-07-20 15:26:06 +0200

    test: add unit test for LessonService.createLesson()

--- src/test/java/pl/kamil/content_service/services/LessonServiceTest.java
+++ src/test/java/pl/kamil/content_service/services/LessonServiceTest.java
@@ -1,6 +1,7 @@
 package pl.kamil.content_service.services;
 
 
+import jakarta.validation.constraints.AssertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,14 +10,14 @@ import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.client.RestClientException;
 import pl.kamil.content_service.dtos.FileUploadResponse;
 import pl.kamil.content_service.models.Lesson;
 import pl.kamil.content_service.repositories.LessonRepository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class LessonServiceTest {
@@ -26,7 +27,6 @@ public class LessonServiceTest {
     @Mock
     private LessonFileService lessonFileService;
 
-    @Spy
     @InjectMocks
     private LessonService lessonService;
 
@@ -58,5 +58,53 @@ public class LessonServiceTest {
 
     }
 
-    // Invalid or null file. Should thrown
+    // Invalid or null file. Should have thrown
+    @Test
+    void shouldThrowIllegalArgumentException_WhenFileIsNull() {
+        MockMultipartFile mockFile = null;
+
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> lessonService.createLesson(mockFile, 1L));
+
+        assertTrue(exception.getMessage().contains("File must not be null or empty"));
+    }
+
+    @Test
+    void shouldThrowException_WhenFileIsEmpty() {
+        MockMultipartFile mockFile = new MockMultipartFile(
+                "file",
+                "name.txt",
+                "text/plain",
+                new byte[0]
+        );
+
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> lessonService.createLesson(mockFile, 1L));
+
+        assertTrue(exception.getMessage().contains("File must not be null or empty"));
+    }
+
+    @Test
+    void shouldThrowException_WhenProblemWithUploading() {
+        // Given
+        MockMultipartFile mockFile = new MockMultipartFile(
+                "file",
+                "test.txt",
+                "text/plain",
+                "Hello, World!".getBytes()
+        );
+        long userId = 1L;
+        when(lessonFileService.uploadFile(any(MockMultipartFile.class)))
+                .thenThrow(new RestClientException("Invalid response from file storage service"));
+
+        // When
+        Exception exception = assertThrows(RestClientException.class, () -> lessonService.createLesson(mockFile, userId));
+
+        // Then
+        assertTrue(exception.getMessage().contains("Invalid response from file storage service"));
+        verify(lessonRepository, never()).save(any());
+        verify(lessonFileService).uploadFile(mockFile);
+    }
+
+
 }
