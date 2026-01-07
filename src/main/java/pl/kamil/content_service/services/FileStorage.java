28a0f00329043a4e317442b713b31c9dbb7711ea
KamilMad <kamilul085@gmail.com>
2026-01-07 22:29:12 +0100

    refactor: remove unnecessary modifiers

--- src/main/java/pl/kamil/content_service/services/FileStorage.java
+++ src/main/java/pl/kamil/content_service/services/FileStorage.java
@@ -1,12 +1,11 @@
 package pl.kamil.content_service.services;
 
-import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 import pl.kamil.content_service.dtos.FileUploadResponse;
 
 public interface FileStorage {
 
-    public FileUploadResponse storeFile(MultipartFile file);
-    public void deleteFile(String key);
-    public String getFileContent(String fileKey);
+    FileUploadResponse storeFile(MultipartFile file);
+    void deleteFile(String key);
+    String getFileContent(String fileKey);
 }
