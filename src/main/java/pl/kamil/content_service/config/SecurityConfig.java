6172528fd3dcc8252defe22eec3f8e27f2d96e90
KamilMad <kamilul085@gmail.com>
2026-01-12 14:26:13 +0100

    refactor: rename securitFilterChain and delete JwtDecoder

--- src/main/java/pl/kamil/content_service/config/SecurityConfig.java
+++ src/main/java/pl/kamil/content_service/config/SecurityConfig.java
@@ -1,21 +1,17 @@
 package pl.kamil.content_service.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    @Value("${jwk.set.uri}")
-    private String jwkSetUri;
-
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain contentSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated())
@@ -24,7 +20,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
-    }
 }
