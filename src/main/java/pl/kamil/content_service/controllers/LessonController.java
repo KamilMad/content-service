134f3034961c4f86acc4c7ffffb9a59615b21118
KamilMad <kamilul085@gmail.com>
2026-02-08 19:02:00 +0100

    feat: enable logging and clean up CORS annotation

--- src/main/java/pl/kamil/content_service/controllers/LessonController.java
+++ src/main/java/pl/kamil/content_service/controllers/LessonController.java
@@ -2,6 +2,7 @@ package pl.kamil.content_service.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://127.0.0.1:5500")  // Allow frontend access
+@Slf4j
 @RestController
 @RequestMapping("/lessons")
 @RequiredArgsConstructor
@@ -30,6 +31,7 @@ public class LessonController {
             @CurrentUserId UUID userId,
             @Valid @ModelAttribute  FileUploadRequest request) throws IOException {
 
+        log.info("Controller : User id is {}", userId);
          LessonResponse response =  lessonService.createLesson(request.file(), userId);
 
          URI location = ServletUriComponentsBuilder
@@ -43,7 +45,7 @@ public class LessonController {
 
     @GetMapping
     public ResponseEntity<LessonsResponse> getLessons(
-            @RequestHeader("X-User-Id")  UUID userId) {
+            @CurrentUserId  UUID userId) {
 
          LessonsResponse response = lessonService.getAllLessons(userId);
         return ResponseEntity.ok().body(response);
@@ -52,7 +54,7 @@ public class LessonController {
     @GetMapping("/{id}")
     public ResponseEntity<LessonResponse> getLessonById(
             @PathVariable("id")  UUID lessonId,
-            @RequestHeader("X-User-Id")  UUID userId) {
+            @CurrentUserId UUID userId) {
 
          LessonResponse response = lessonService.getLesson(lessonId, userId);
         return ResponseEntity.ok().body(response);
@@ -61,7 +63,7 @@ public class LessonController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLesson(
             @PathVariable("id")  UUID lessonId,
-            @RequestHeader("X-User-Id")  UUID userId) {
+            @CurrentUserId UUID userId) {
 
         lessonService.deleteLesson(lessonId, userId);
         return ResponseEntity.noContent().build();
@@ -70,7 +72,7 @@ public class LessonController {
     @GetMapping("/{id}/content")
     public ResponseEntity<LessonContentResponse> getContent(
             @PathVariable("id")  UUID lessonId,
-            @RequestHeader("X-User-Id")  UUID userId) {
+            @CurrentUserId UUID userId) {
 
          LessonContentResponse contentResponse = lessonService.getLessonContent(lessonId, userId);
         return ResponseEntity.ok().body(contentResponse);
