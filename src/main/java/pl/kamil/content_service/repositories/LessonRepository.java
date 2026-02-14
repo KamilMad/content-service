6046a648933d2f0f3c2ac9ae343a15f6561c3aa4
KamilMad <kamilul085@gmail.com>
2026-02-14 01:55:33 +0100

    chore: remove unused query

--- src/main/java/pl/kamil/content_service/repositories/LessonRepository.java
+++ src/main/java/pl/kamil/content_service/repositories/LessonRepository.java
@@ -6,13 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.kamil.content_service.models.Lesson;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, UUID> {
 
     Page<Lesson> findAllByCreatedBy(UUID userId, Pageable pageable);
-    Optional<Lesson> findByIdAndCreatedBy(UUID lessonId, UUID userId);
-
 }
