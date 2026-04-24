940f4126b93ac3495c35d24ecb552ca8b1ca013d
KamilMad <kamilul085@gmail.com>
2026-04-24 18:37:53 +0200

    feat: add Spring Actuator and enable health endpoint

--- src/main/java/pl/kamil/content_service/config/SecurityConfig.java
+++ src/main/java/pl/kamil/content_service/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
